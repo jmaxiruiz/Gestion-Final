147aae26ff39b3971f17071386b462e65b0618a2
maxi <maxi@192.168.1.25>
2018-03-01 18:11:27 -0300

    Creo clase Controlador_Cliente con un metodo

---
+++ src/controlador/Controlador_Cliente.java
@@ -0,0 +1,18 @@
+
+package controlador;
+
+import modelo.Cliente;
+
+/**
+ *
+ * @author maxi
+ */
+public class Controlador_Cliente {
+    
+    private Cliente cliente;
+    
+    public void hola(){
+        System.out.println("Soy el metodo hola");
+    }
+    
+}
