57c76aefcad344fd7e060e7c10977c01a965a136
maxi <maxi@192.168.1.25>
2018-03-01 16:54:44 -0300

    clase principal, mensaje de hola

---
+++ src/final_gestion_tienda/Final_Gestion_Tienda.java
@@ -0,0 +1,17 @@
+
+package final_gestion_tienda;
+
+/**
+ *
+ * @author maxi
+ */
+public class Final_Gestion_Tienda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
